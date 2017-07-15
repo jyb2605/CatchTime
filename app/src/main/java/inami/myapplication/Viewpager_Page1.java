65b71b0d8fde8ce52a92e60b891d33a4730ba0d1
Hanul <nul010@naver.com>
2017-07-16 03:56:10 +0900

    화면전환

---
+++ app/src/main/java/inami/myapplication/Viewpager_Page1.java
@@ -0,0 +1,28 @@
+package inami.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.anandmuralidhar.simplearandroid.R;
+
+public class Viewpager_Page1 extends android.support.v4.app.Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        LinearLayout linearLayout = (LinearLayout)inflater.inflate(R.layout.viewpager_child,container,false);
+
+        ImageView tutorial = (ImageView) linearLayout.findViewById(R.id.img_tutorial);
+//        tutorial.setImageResource(R.drawable.tu1);
+
+        return linearLayout;
+    }
+}
