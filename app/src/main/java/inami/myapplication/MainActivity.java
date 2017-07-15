b851595367dbb57d81d3fe9be79ae286ae85e65f
완빈 <a@ab.c>
2017-07-15 23:20:35 +0900

    wanbeen

--- app/src/main/java/inami/myapplication/MainActivity.java
+++ app/src/main/java/inami/myapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.anandmuralidhar.simplearandroid.R;
 import com.anandmuralidhar.simplearandroid.SimpleARActivity;
@@ -11,18 +14,49 @@ import com.anandmuralidhar.simplearandroid.SimpleARActivity;
 public class MainActivity extends AppCompatActivity {
 
 
+    ImageButton logbtn1;
+    ImageButton logbtn2;
+    ImageButton logbtn3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Handler hd = new Handler();
-        hd.postDelayed(new Runnable() {
+        setContentView(R.layout.mainpage_activity);
 
-            @Override
-            public void run() {
-                finish();
-                startActivity(new Intent(MainActivity.this, SimpleARActivity.class));
-            }
-        }, 1000);
+        findViewById(R.id.logbtn1).setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent1 = new Intent(MainActivity.this,SplashActivity.class);
+                        startActivity(intent1);
+                    }
+                }
+        );
+
+        findViewById(R.id.logbtn2).setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent1 = new Intent(MainActivity.this,SplashActivity.class);
+                        startActivity(intent1);
+                    }
+                }
+        );
+
+        findViewById(R.id.logbtn3).setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent1 = new Intent(MainActivity.this,SplashActivity.class);
+                        startActivity(intent1);
+                    }
+                }
+        );
+
+        findViewById(R.id.skipbtn).setOnClickListener(
+                new ImageButton.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent1 = new Intent(MainActivity.this,SplashActivity.class);
+                        startActivity(intent1);
+                    }
+                }
+        );
     }
 }
