10a0170fd858cb617905f0704aa3d690d71a3de8
JINMO\Jimmo__ <zlzl9879@gmail.com>
2017-07-15 20:37:57 +0900

    Splash

---
+++ app/src/main/java/inami/myapplication/SplashActivity.java
@@ -0,0 +1,34 @@
+package inami.myapplication;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+
+import com.anandmuralidhar.simplearandroid.R;
+
+public class SplashActivity extends AppCompatActivity{
+    LinearLayout layout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        layout = (LinearLayout) findViewById(R.id.layout);
+
+        final Animation animTransfadeout = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.fadein);
+        layout.startAnimation(animTransfadeout);
+        Handler hd = new Handler();
+        hd.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 3000);
+    }
+}
